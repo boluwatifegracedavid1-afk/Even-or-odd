6f8ef4b37a166e85c81356f6b00904f7278e1a90
Boluwatife David <boluwatifegracedavid@gmail.com>
2026-02-02 03:26:51 -0800

    upload only if_and_elif.py

---
+++ if_and_elif.py
@@ -0,0 +1,37 @@
+# understanding the importance of else and elif
+"""
+x= int(input ("Type a value for x: "))
+y= int(input ("Type a value for y: "))
+
+if x<y :
+    print (x,"is less than", y)
+if x>y :
+    print (x,"is greater than", y)
+if x==y :
+    print (x,"is equal to", y)
+"""
+
+"""
+x= int(input ("Type a value for x: "))
+y= int(input ("Type a value for y: "))
+
+if x<y :
+    print (x,"is less than", y)
+elif x>y :
+    print (x,"is greater than", y)
+else :
+    print (x,"is equal to", y)
+"""
+
+"""
+x= int(input ("Type a value for x: "))
+y= int(input ("Type a value for y: "))
+
+if x==y :
+    print (x,"is equal to", y)
+else :
+    print (x,"is not equal to", y)
+"""
+
+
+
